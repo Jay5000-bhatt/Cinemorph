e2df32ff97ee1d94244352b1de0fd94c4e13f22b
Jay Bhatt <78402661+Jay5000-bhatt@users.noreply.github.com>
2024-03-08 12:58:18 +0530

    Update discussionComment.jsx

--- client/src/components/discussionSection/discussionComment.jsx
+++ client/src/components/discussionSection/discussionComment.jsx
@@ -67,7 +67,7 @@ const DiscussionComment = ({ video, crew }) => {
       const tmdbId = segments[segments.length - 2];
 
       const response = await axios.post(
-        "https://cinemorph-api.vercel.app/discuss/comments",
+        "https://cinemorph-api-git-master-jay-bhatts-projects.vercel.app/discuss/comments",
         {
           tmdbId,
           userId: userName,
@@ -93,7 +93,7 @@ const DiscussionComment = ({ video, crew }) => {
       const segments = path.split("/");
       const tmdbId = segments[segments.length - 2];
       const response = await axios.get(
-        `https://cinemorph-api.vercel.app/discuss/posts/${tmdbId}/comments`
+        `https://cinemorph-api-git-master-jay-bhatts-projects.vercel.app/discuss/posts/${tmdbId}/comments`
       );
 
       setComments(response.data);
@@ -112,7 +112,7 @@ const DiscussionComment = ({ video, crew }) => {
       const segments = path.split("/");
       const tmdbId = segments[segments.length - 2];
       const response = await axios.post(
-        "https://cinemorph-api.vercel.app/discuss/comments/reply",
+        "https://cinemorph-api-git-master-jay-bhatts-projects.vercel.app/discuss/comments/reply",
         {
           tmdbId,
           userId: userName,
